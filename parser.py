12ae98bc476fbb408e78bb32bea591e688b06a83
itisnanful <itisnanful@gmail.com>
2015-05-10 15:34:26 -0700

    ss

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import ujson
 import sqlite3
-from datetime import datettime
+from datetime import datetime
+from pprint import pprint
 
 def GetData(table_name):
     conn = sqlite3.connect('participants.db')
@@ -13,104 +14,96 @@ def GetData(table_name):
         data.append(s)
     return data
 
-def ParseOne(datarow):
-    assignmentID = datarow['assignmentId']
+def ParseOne(datarow, taskID):
+    assignmentID = taskID
     gender = datarow['questiondata']['gender']
     age = datarow['questiondata']['age']
     demographic = datarow['questiondata']['demographic']
     handedness = datarow['questiondata']['handedness']
     starttime = datetime.fromtimestamp(
-        int(datarow['data'][0]['datatime'])
+        int(datarow['data'][0]['dateTime']/1000)
     ).strftime('%Y-%m-%d %H:%M:%S')
 
-    triplet1s = []
+    triplets = []
     doublets = []
-    triplet2s = []
     for i in range(0, len(datarow['data'])):
         if datarow['data'][i]['trialdata']['phase'] == 'Triplet1':
-            triplet1s.append(datarow['data'][i]['trialdata'])
-        if datarow['data'][i]['trialdata']['phase'] == 'Doublets':
+            triplets.append(datarow['data'][i]['trialdata'])
+        if datarow['data'][i]['trialdata']['phase'] == 'Doublet':
             doublets.append(datarow['data'][i]['trialdata'])
         if datarow['data'][i]['trialdata']['phase'] == 'Triplet2':
-            triplet2s.append(datarow['data'][i]['trialdata'])
+            triplets.append(datarow['data'][i]['trialdata'])
 
-    triplet1sdata = []
-    for i in range(0, len(triplet1s)):
+    tripletsdata = []
+    for i in range(0, len(triplets)):
         arr = []
-        arr.append(triplet1s[i]['topimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(triplet1s[i]['leftimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(triplet1s[i]['rightimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(triplet1s[i]['response'])
-        arr.append(triplet1s[i]['reacttime'])
+        arr.append(str(triplets[i]['topimg']\
+            .split('/static/images/EclipseFace/')[1].split('.png')[0]))
+        arr.append(str(triplets[i]['leftimg']\
+            .split('/static/images/EclipseFace/')[1].split('.png')[0]))
+        arr.append(str(triplets[i]['rightimg']\
+            .split('/static/images/EclipseFace/')[1].split('.png')[0]))
+        arr.append(str(triplets[i]['response']))
+        arr.append(str(triplets[i]['reacttime']))
 
         arr.append(assignmentID)
-        arr.append(gender)
-        arr.append(age)
-        arr.append(demographic)
-        arr.append(handedness)
+        arr.append(str(gender))
+        arr.append(str(age))
+        arr.append(str(demographic))
+        arr.append(str(handedness))
         arr.append(starttime)
+        if triplets[i]['phase'] == 'Triplet1':
+            arr.append('Triplet1')
+        if triplets[i]['phase'] == 'Triplet2':
+            arr.append('Triplet2')
 
-        triplet1sdata.append(arr)
+        tripletsdata.append(arr)
 
     doubletsdata = []
     for i in range(0, len(doublets)):
         arr = []
-        arr.append(doublets[i]['leftimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(doublets[i]['rightimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(doublets[i]['response'])
-        arr.append(doublets[i]['reacttime'])
+        arr.append(str(doublets[i]['leftimg']\
+            .split('/static/images/EclipseFace/')[1].split('.png')[0]))
+        arr.append(str(doublets[i]['rightimg']\
+            .split('/static/images/EclipseFace/')[1].split('.png')[0]))
+        arr.append(str(doublets[i]['response']))
+        arr.append(str(doublets[i]['reacttime']))
 
         arr.append(assignmentID)
-        arr.append(gender)
-        arr.append(age)
-        arr.append(demographic)
-        arr.append(handedness)
+        arr.append(str(gender))
+        arr.append(str(age))
+        arr.append(str(demographic))
+        arr.append(str(handedness))
         arr.append(starttime)
         
         doubletsdata.append(arr)
 
-    triplet2sdata = []
-    for i in range(0, len(triplet2s)):
-        arr = []
-        arr.append(triplet2s[i]['topimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(triplet2s[i]['leftimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(triplet2s[i]['rightimg']\
-            .split('/static/images/EclipseFace/')[1].split('.png')[0])
-        arr.append(triplet2s[i]['response'])
-        arr.append(triplet2s[i]['reacttime'])
+    return [tripletsdata, doubletsdata]
 
-        arr.append(assignmentID)
-        arr.append(gender)
-        arr.append(age)
-        arr.append(demographic)
-        arr.append(handedness)
-        arr.append(starttime)
-        
-        triplet2sdata.append(arr)
-
-    return [triplet1sdata, doubletsdata, triplet2sdata]
+def writeListToFile(theList, fileName):
+    with open(fileName, "w") as ins:
+        for item in theList:
+            ins.write(str(item) + '\n')
 
 if __name__=='__main__':
     #data = ujson.dumps(f.read())
     data = GetData('turkdemo')
-    triplet1sdata = []
+    tripletsdata = []
     doubletsdata = []
-    triplet2sdata = []
-    for datarow in data:
-        tup = ParseOne(datarow)
-        triplet1sdata.append(tup[0])
-        doubletsdata.append(tup[1])
-        triplet2sdata.append(tup[2])
-    print triplet1sdata
+    taskID = 1
+    for row in data:
+        datarow = ujson.loads(row)
+        tup = ParseOne(datarow, taskID)
+        for item in tup[0]:
+            tripletsdata.append(item)
+        for item in tup[1]:
+            doubletsdata.append(item)
+        # tripletsdata.extend(tup[0])
+        # doubletsdata.extend(tup[1])
+        taskID = taskID + 1
 
-
+    writeListToFile(tripletsdata, 'output/triplets.txt')
+    writeListToFile(doubletsdata, 'output/doublets.txt')
 
 
 
